597bccdb83a47f1a711e2d6bf079b8549cecd4d6
Edgar Sanchez Gamez <115099637+EdgarSanchezGamez@users.noreply.github.com>
2022-10-11 16:43:47 +0200

    Add files via upload

---
+++ MasterMind/src/mastermind/MastermindView.java
@@ -0,0 +1,34 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package mastermind;
+
+import interfaces.MasterMindViewContract;
+import interfaces.Matches;
+import java.util.Map;
+
+/**
+ *
+ * @author i7 6700k
+ */
+public class MastermindView implements MasterMindViewContract{
+
+    @Override
+    public void winGame(String a) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void overGame(String a) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void play(Map<Matches, Integer> mapa) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+    
+    
+}

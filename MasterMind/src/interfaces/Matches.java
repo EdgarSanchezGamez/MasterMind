0ed4039893e24f518b58abcc35be54a164e09796
Edgar Sanchez Gamez <115099637+EdgarSanchezGamez@users.noreply.github.com>
2022-10-11 16:03:08 +0200

    Interfaces

---
+++ MasterMind/src/interfaces/Matches.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Enum.java to edit this template
+ */
+package interfaces;
+
+/**
+ *
+ * @author i7 6700k
+ */
+public enum Matches {
+    Ok,wrongPosition 
+}
---
+++ MasterMind/src/mastermind/GameListener.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package mastermind;
+
+import interfaces.GameListenerContract;
+
+/**
+ *
+ * @author i7 6700k
+ */
+public class GameListener implements GameListenerContract{
+
+    @Override
+    public void over() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void win() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+    
+    
+}
